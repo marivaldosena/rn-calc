7dbe42d20b20537c160f6f8671c1e40e48a857b4
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-24 15:19:47 -0300

    Criar boilerplate dos componentes Topo, Resultado e Painel.

---
+++ src/components/Painel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Painel = () => {
+  return (
+    <View>
+      <Text>Painel</Text>
+    </View>
+  );
+};
+
+export default Painel;
---
+++ src/components/Topo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Topo = () => {
+  return (
+    <View>
+      <Text>Topo.js</Text>
+    </View>
+  );
+};
+
+export default Topo;
