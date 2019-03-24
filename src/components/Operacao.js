0e2273d81ff9c45953cb9f7b2e02e2c0e5e829b1
Marivaldo Sena <msena.ifsp@gmail.com>
2019-03-24 19:03:53 -0300

    Criar funcionalidade de selecionar operação.

--- src/components/Operacao.js
+++ src/components/Operacao.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Picker, StyleSheet } from 'react-native';
 
-const Operacao = () => (
-  <View>
-    <Text>Operação</Text>
-  </View>
-);
+const Operacao = () => {
+  const { principal } = estilos;
+
+  return (
+    <Picker style={principal}>
+      <Picker.Item label='Soma' value='soma' />
+      <Picker.Item label='Subtração' value='subtracao' />
+      <Picker.Item label='Multiplicação' value='multiplicacao' />
+      <Picker.Item label='Divisão' value='divisao' />
+    </Picker>
+  );
+};
+
+const estilos = StyleSheet.create({
+  principal: {
+    marginTop: 15,
+    marginBottom: 15,
+  }, 
+});
 
 export default Operacao;
